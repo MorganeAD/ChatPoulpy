a4e0a24f8c4dc5ba95ab88a6e15ee60d2fafe5fa
Morgane Alonso <ptitenoobeuh@hotmail.fr>
2013-01-07 00:00:25 +0100

    mise en place plateforms table

--- main.lua
+++ main.lua
@@ -8,18 +8,30 @@ function love.load()
     --love.sensor.enable("TYPE_ACCELEROMETER")
     --acc = 0
     c = Character:new()
-    camera = Camera:new()
-    p1 = Plateform:new(100, 100)
-    p2 = Plateform:new(250, 250)
-    p3 = Plateform:new(200, 400)
+    camera = Camera:new()    
+
+    plateforms = {}
+
+    for i = 1, 20 do
+        for j = 1, 10 do
+            table.insert(plateforms, Plateform:new(math.random(0 + 50, 320 - 50),math.random(480, -480)))
+        end
+    end
+
+    --table.insert(plateforms, Plateform:new(100, 100))
+    --table.insert(plateforms, Plateform:new(250, 250))
+    --table.insert(plateforms, Plateform:new(200, 400))
+
 end
 
 function love.update(dt)
+
     c:update(dt)
     camera:update(dt)
-    p1:update(dt)
-    p2:update(dt)
-    p3:update(dt)
+    for _, p in pairs(plateforms) do
+        p:update(dt)
+    end    
+
 end
 
 function love.sensorchanged(n,t,v)
@@ -28,9 +40,9 @@ end
 
 function love.draw()
     camera:set()
+    for _, p in pairs(plateforms) do
+        p:draw()
+    end
     c:draw()
-    p1:draw()
-    p2:draw()
-    p3:draw()
     camera:unset()
 end
