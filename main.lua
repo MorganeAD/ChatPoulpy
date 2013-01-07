0013e532621f4b7cca285b829e568e6aad3c3103
Niouby <ptitenoobeuh@hotmail.fr>
2013-01-07 20:33:44 +0100

    création infini des plateforms

--- main.lua
+++ main.lua
@@ -9,25 +9,33 @@ function love.load()
     --acc = 0
     c = Character:new()
     camera = Camera:new()
-
+    b = 500
     plateforms = {}
+    birth()
     
-    for i = 1, 1000 do
-        x = math.random(0, 20)
-        if x >= 3 and x <= 20 then table.insert(plateforms, Plateform:new(math.random(0 + 50, 320 - 50), - i*30 + 480)) end
-    end    
+    --for i = 1, 1000 do
+    --    x = math.random(0, 20)
+    --    if x >= 3 and x <= 20 then table.insert(plateforms, Plateform:new(math.random(0 + 50, 320 - 50), - i*30 + 480)) end
+    --end    
 
 end
 
 function love.update(dt)
 
-
+    b = b + camera.y
     c:update(dt)
     camera:update(dt)
     for _, p in pairs(plateforms) do
         p:update(dt)
     end    
+end
 
+function birth()
+
+    for i = 1, b do
+        x = math.random(0, 20)
+        if x >= 3 and x <= 20 then table.insert(plateforms, Plateform:new(math.random(0 + 50, 320 - 50), - i*30 + 480)) end
+    end
 end
 
 function love.sensorchanged(n,t,v)
